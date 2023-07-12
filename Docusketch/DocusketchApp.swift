f97e97b9084330328c025edd1297c7e165f205ba
Sad Tyoma <artyomshuneyko@gmail.com>
2023-07-12 19:24:30 +0300

    Initial Commit

---
+++ Docusketch/DocusketchApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocusketchApp.swift
+//  Docusketch
+//
+//  Created by Artem Shuneyko on 12.07.23.
+//
+
+import SwiftUI
+
+@main
+struct DocusketchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
